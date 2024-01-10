0a0acb3e44b84950f0ceec5fa3c9765e6b1be7f7
remiodufuye <remi.odufuye@gmail.com>
2024-01-10 18:24:52 -0500

    Genres As Static Data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 
 
-import useData from "./useData";
+import genres from '../data/genres'
 
 
 export interface Genre {
@@ -11,6 +11,10 @@ export interface Genre {
 }
 
 
-const useGenres = () => useData<Genre>('/genres')
+const useGenres = () => ({
+    data:genres , 
+    isLoading:false,
+    error:null
+})
 
 export default useGenres
